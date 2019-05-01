ee3665ab5cf70ca28e01567d5f82153b7a386ebd
Hector Perea <hperea.alfa@gmail.com>
2019-05-01 17:27:09 -0500

    Make a component and retrieve data from it to the instance

--- components.js
+++ components.js
@@ -2,33 +2,55 @@
 /* eslint-disable no-unused-vars */
 
 /* Global component */
-Vue.component("simple_comp", {
-  template: "#temp",
+// Vue.component("simple_comp", {
+//   template: "#temp",
+//   data() {
+//     return {
+//       message: "I don't have props. yet" + " " + this.prop
+//     };
+//   },
+//   props: ["prop"],
+//   methods: {}
+// });
+
+Vue.component("card", {
+  template: "#nw_card",
   data() {
     return {
-      message: "I don't have props. yet" + " " + this.prop
+      title: "Light Words",
+      msg: "The best thing in life is the ones that last forever. Never give up on your dreams."
     };
   },
-  props: ["prop"],
+  props: [],
+  methods: {
+    send() {
+      this.$emit("relay",this.title,this.msg);
+    }
+  }
+});
+
+Vue.component("card_2", {
+  template: "#nw_card_2",
+  data() {
+    return {
+      heading: "Heading from component 1",
+      new_msg: "Content from component 1"
+    };
+  },
+  props: [],
   methods: {}
 });
 
 const vm = new Vue({
   el: "#app",
   data: {
-    vue_data: "but now i do!!"
+    heading: "Waiting for data component",
+    new_msg: "Waiting for data component"
   },
-  /* Local component */
-  components: {
-    nw_comp: {
-      template: "#temp2",
-      data() {
-        return {
-          msg: "This is a local component"
-        };
-      },
-      props: [],
-      methods: {}
+  methods: {
+    allocate(heading, msg) {
+      this.heading = heading;
+      this.new_msg = msg;
     }
   }
 });
